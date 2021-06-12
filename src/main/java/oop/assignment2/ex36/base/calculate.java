385a14c99fb841a8aad3a8f63a31bbe5244d116b
Akwill47 <wil.weat@hotmail.com>
2021-06-12 08:49:57 -0400

    ONE OF US

--- src/main/java/oop/assignment2/ex36/base/calculate.java
+++ src/main/java/oop/assignment2/ex36/base/calculate.java
@@ -5,22 +5,37 @@ import java.util.ArrayList;
 public class calculate {
     public double average(ArrayList<Integer> statsList){
         double average=0;
+        double sum=0;
+        for(int i=0;i<statsList.size();i++){
+            sum+=statsList.get(i);
+        }
+        average = sum/statsList.size();
 
         return average;
     }
     public double max(ArrayList<Integer> statsList){
-        double max=0;
+        double max=statsList.get(0);
+        for(int i=0;i<statsList.size();i++){
+            if(max < statsList.get(i))
+            max = statsList.get(i);
+        }
 
         return max;
     }
-    public double min(ArrayList<Integer> statsList){
-        double min=0;
-
+    public double min(ArrayList<Integer> statsList) {
+        double min = statsList.get(0);
+        for (int i = 0; i < statsList.size(); i++) {
+            if (min > statsList.get(i))
+                min = statsList.get(i);
+        }
         return min;
     }
-    public double std(ArrayList<Integer> statsList){
+    public double std(ArrayList<Integer> statsList,double mean) {
         double std=0;
-
+        for (int i = 0; i < statsList.size(); i++) {
+            std += Math.pow(statsList.get(i) - mean, 2);
+        }
+        std = Math.sqrt(std/statsList.size());
         return std;
     }
 }
--- src/main/java/oop/assignment2/ex36/base/print.java
+++ src/main/java/oop/assignment2/ex36/base/print.java
@@ -1,4 +1,15 @@
 package oop.assignment2.ex36.base;
 
+import java.util.ArrayList;
+
 public class print {
+    public void display(ArrayList<Integer> statsList,double max, double min, double std, double average){
+        for(int i: statsList){
+            System.out.print(i);
+        }
+        System.out.printf("The average is : %.1lf",average);
+        System.out.printf("The minimum is : %d",min);
+        System.out.printf("The maximum is : %d",max);
+        System.out.printf("The standard deviation is : %.2lf",std);
+    }
 }
